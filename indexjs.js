8c0f9493a63a02fd3bc72bcfaffde942e1a485dd
Mathushanan Mahendrarajah <posttoday0@gmail.com>
2023-07-11 00:21:39 +0530

    responsive navbar created & social icons added

--- indexjs.js
+++ indexjs.js
@@ -10,10 +10,28 @@ let navlist = document.querySelector('.navlist');
 let closeBar = document.querySelector('.close-bar');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx');
-    navlist.classList.toggle('open');
-    closeBar.classList.toggle('close-bar');
+  menu.classList.toggle('bx');
+  navlist.classList.toggle('open');
+  
+  if (menu.classList.contains('bx')) {
+    menu.classList.remove('fa-bars');
+    menu.classList.add('fa-bars-staggered','fa-fade');
+    navlist.classList.add('mobile-navlist','shadow-lg');
+  } else {
+    menu.classList.remove('fa-bars-staggered','fa-fade');
+    menu.classList.add('fa-bars');
+    navlist.classList.remove('mobile-navlist','shadow-lg');
+  }
 };
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 890) {
+      navlist.classList.remove('mobile-navlist','shadow-lg');
+      menu.classList.remove('fa-bars-staggered','fa-fade');
+      menu.classList.add('fa-bars');
+    }
+  });
+
 
 
 
